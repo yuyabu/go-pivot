b1ecf5e6cac010d43d372a004771af0330e581e3
Yuhei Yabushita <yabushita@nautilus-technologies.com>
2019-07-22 22:58:07 +0900

    first version

--- pivot.go
+++ pivot.go
@@ -1,27 +1,38 @@
 package main
 
 import (
-	"bufio"
-	"flag"
 	"fmt"
 	"os"
+	//"string"
+	"encoding/csv"
 )
 
-var sc = bufio.NewScanner(os.Stdin)
 
 func main() {
-	var (
-		delimiter = flag.String("str", "default", "string flag")
-	)
-	flag.Parse()
-	fmt.Println(*delimiter)
-	var s, t string
-	if sc.Scan() {
-		s = sc.Text()
-	}
-	if sc.Scan() {
-		t = sc.Text()
-	}
-	fmt.Println(s)
-	fmt.Println(t)
+	fmt.Println(os.Args[1])
+	//table := [][]string{}
+	csvFile, err := os.Open(os.Args[1])
+         if err != nil {
+                 fmt.Println(err)
+         }
+
+	reader := csv.NewReader(csvFile)
+	reader.Comma = '\t'
+	reader.FieldsPerRecord = -1
+
+	csvData, err := reader.ReadAll()
+	         if err != nil {
+                 fmt.Println(err)
+                 os.Exit(1)
+	 }
+	 fmt.Println(csvData)
+	 m := len(csvData)
+	 n := len(csvData[1])
+	 for i:=0; i< n; i++{
+		 for j :=0 ; j<m ;j++ {
+			fmt.Printf(csvData[j][i] + ",")
+		}
+		fmt.Println()
+	 }
 }
+
